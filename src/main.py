c949d60cd40d58826468179b994c0bbdc32d714d
Gustavo <gugaswhom@gmail.com>
2015-12-26 16:40:33 +0000

    Created the main script for CLI.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    """
+        Main function for the execution of the main CLI project for the birdy
+        project.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
