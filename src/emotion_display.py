6110e6dcd93ee32603f6e7c1e0f178ee9b59d2fd
Gustavo <gugaswhom@gmail.com>
2016-01-09 11:04:48 +0000

    Added more functionality to the layout.
    Added more functionality to take up the actions of listing, previous and
    next actions to iterate throught the tweet list.

--- src/emotion_display.py
+++ src/emotion_display.py
@@ -3,7 +3,10 @@
     This is the file that will have the text and the results of the sentiment
     analysis.
 """
+from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+
+# from emotional import sentient
 
 class TextLayout(BoxLayout):
     """
@@ -13,7 +16,7 @@ class TextLayout(BoxLayout):
     """
     pass
 
-class ResultsLLayout(BoxLayout):
+class ResultsLayout(BoxLayout):
     """
         This class will represent the other adjacent part of the screen that
         will give out to the user the values that the text in question got as
@@ -27,4 +30,55 @@ class SentimentalLayout(BoxLayout):
         and the Results. They should go side by side for better occupation of
         the horizontal/landscape screen.
     """
+    def getTweets(self, tweets):
+        """
+            Method that will receive a list of tweets and assigns them to the
+            self.tweets attribute, it must be a list of objects.
+        """
+        self.tweets = tweets
+        self.pos = 0
+
+    def nextTweet(self):
+        """
+            Method that will go on to the next tweet. It will be the action of
+            the next button. The class will have a pos attribute to register in
+            which tweet the user is at.
+        """
+        if len(self.tweets) == (self.pos+1):
+            self.pos += 1
+        return self.tweets[self.pos]
+
+    def previousTweet(self):
+        """
+            Method that gets the previous tweet from the tweets attribute list.
+        """
+        if self.pos != 0:
+            self.pos -= 1
+        return self.tweets[self.pos]
+
+    def tweetText(self, tweet):
+        """
+            Method that will dump the JSON tweet and make it more visually
+            appealing to the eye of the user.
+        """
+        text = tweet.author + '\n\n'
+        text += tweet.text + '\n\n'
+        text += tweet.date + '\n'
+        return text
+
+    def analyzeTweet(self):
+        """
+            Method that will handle all of the sentiment analysis. This method
+            will call the lib done for the actual sentiment analysis.
+        """
+        # use the sentient module
+        # print('it reached here')
+        pass
+
     pass
+
+class SentimentApp(App):
+    pass
+
+if __name__ == '__main__':
+    SentimentApp().run()
